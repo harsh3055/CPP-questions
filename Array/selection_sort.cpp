12c93b3ed91a01b56fbf87dcd46ff0fe0ed2bb1e
Harsh Goswami <78903964+harsh3055@users.noreply.github.com>
2024-10-25 19:28:28 +0530

    Create selection_sort.cpp

---
+++ Array/selection_sort.cpp
@@ -0,0 +1,28 @@
+// C++ program for the implementation of Bubble sort
+#include <bits/stdc++.h>
+using namespace std;
+
+void selection_sort(vector<int>& v) {
+    int n = v.size();
+
+    for (int i = 0; i < n - 1; i++) {
+
+      int beginning_index = i;
+      int smallest;
+      for(int j = i+1; j< n; j++){
+          if(v[j]<v[j-1]){
+             smallest = j;
+          }
+      }
+      swap(v[beginning_index], v[smallest]);
+    }
+}
+
+int main() {
+    vector<int> v = {5, 10, 4, 2, 8};
+
+    selection_sort(v);
+    for (auto i : v)
+        cout << i << " ";
+    return 0;
+}
